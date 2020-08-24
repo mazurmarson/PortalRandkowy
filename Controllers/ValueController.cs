6f667f2dc8d49f5b08a94fd6d28714353c41fe9d
Marek Mazur <mazi270tv@gmail.com>
2020-08-24 20:45:18 +0200

    Zamiana metod na asynchroniczne

--- Controllers/ValueController.cs
+++ Controllers/ValueController.cs
@@ -6,6 +6,7 @@ using PortalRandkowy.API.Models;
 using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace PortalRandkowy.API.Controllers
 {
@@ -22,47 +23,47 @@ namespace PortalRandkowy.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetValues()
+        public async Task<IActionResult> GetValues()
         {
-            var values = _context.Values.ToList();
-            return Ok(values);
+            var values = await _context.Values.ToListAsync();
+             return Ok(values);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
             return Ok(value);
         }
 
         [HttpPost]
-        public IActionResult AddValue([FromBody]Value value)
+        public async Task<IActionResult> AddValue([FromBody]Value value)
         {
             _context.Values.Add(value);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return Ok(value);
         }
 
         [HttpPut("{id}")]
-        public IActionResult EditValue(int id,[FromBody]Value value)
+        public async Task<IActionResult> EditValue(int id,[FromBody]Value value)
         {
-            var data = _context.Values.Find(id);
+            var data = await _context.Values.FindAsync(id);
             data.Name = value.Name;
             _context.Values.Update(data);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return Ok(data);
         }
 
         [HttpDelete("{id}")]
-        public IActionResult DeleteValue(int id)
+        public async Task<IActionResult>  DeleteValue(int id)
         {
-            var data = _context.Values.Find(id);
+            var data = await _context.Values.FindAsync(id);
             if(data == null)
             {
                 return NoContent();
             }
             _context.Values.Remove(data);
-            _context.SaveChanges();
+           await _context.SaveChangesAsync();
             return Ok(data);
         }
 
