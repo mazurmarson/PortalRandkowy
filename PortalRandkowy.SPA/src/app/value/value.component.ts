27d3d5051f9e1862e3c4c14deede50625c8ea33d
Marek Mazur <mazi270tv@gmail.com>
2020-08-25 14:36:55 +0200

    Utworzenie klasy user

--- PortalRandkowy.SPA/src/app/value/value.component.ts
+++ PortalRandkowy.SPA/src/app/value/value.component.ts
@@ -12,10 +12,12 @@ export class ValueComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
+  // tslint:disable-next-line: typedef
   ngOnInit() {
     this.getValues();
   }
 
+  // tslint:disable-next-line: typedef
   getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
