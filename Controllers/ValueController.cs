935db36178a0e3989224e99f7c684f4558d0ee76
Marek Mazur <mazi270tv@gmail.com>
2020-08-30 19:16:50 +0200

    Zepsucie projektu

--- Controllers/ValueController.cs
+++ Controllers/ValueController.cs
@@ -23,7 +23,7 @@ namespace PortalRandkowy.API.Controllers
         {
              _context = context;
         }
-
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
---
+++ PortalRandkowy.SPA/src/app/register/register.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  model: any = {};
+
+  constructor() { }
+
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+  }
+
+  // tslint:disable-next-line: typedef
+  register()
+  {
+    console.log(this.model);
+  }
+
+  // tslint:disable-next-line: typedef
+  cancel()
+  {
+    console.log('Anulowane');
+  }
+
+}
