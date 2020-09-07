2d19f613707745f6dd230e2548c4c3137f64de2b
Marek Mazur <mazi270tv@gmail.com>
2020-09-07 15:48:06 +0200

    Edycja użytkownika wykorzystanie API

--- PortalRandkowy.SPA/src/app/users/user-edit/user-edit.component.ts
+++ PortalRandkowy.SPA/src/app/users/user-edit/user-edit.component.ts
@@ -3,6 +3,8 @@ import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -21,7 +23,7 @@ export class UserEditComponent implements OnInit {
     }
   }
 
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService, private userService: UserService, private authService: AuthService) { }
 
   // tslint:disable-next-line: typedef
   ngOnInit() {
@@ -32,10 +34,14 @@ export class UserEditComponent implements OnInit {
 
   updateUser()
   {
-    console.log(this.user);
-    this.alertify.success('Profil pomyślnie zaaktualizowany');
-    this.editForm.reset(this.user); //uzupelnij nowymi danymi
-
-  }
-
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user)
+    .subscribe(next => {
+      this.alertify.success('Profil pomyślnie zaaktualizowany');
+      this.editForm.reset(this.user); //uzupelnij formularz nowymi danymi
+    }, error => {
+      this.alertify.error(error);
+      });
+    }
 }
+
+
